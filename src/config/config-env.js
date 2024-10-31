0d549a98d71dd3ccc231f7ac7e8d6fba4105a280
ByteZO <coder.gny@gmail.com>
2024-10-31 15:35:12 +0530

    Refactor config-env.js for improved readability

--- src/config/config-env.js
+++ src/config/config-env.js
@@ -1,12 +1,9 @@
 // converting env variable to string
 import "dotenv/config.js";
 const envConfig = {
-    DB_URI: String(process.env.MONGO_DB_URI), 
-    PORT: String(process.env.PORT),
-    JWT_SECRET: String(process.env.JWT_SECRET),
+  DB_URI: String(process.env.MONGO_DB_URI),
+  PORT: String(process.env.PORT),
+  JWT_SECRET: String(process.env.JWT_SECRET),
 };
 
-
-
 export default envConfig;
- 
